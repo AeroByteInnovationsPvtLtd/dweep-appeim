c11a0e97e991d5b589dc709201c53593d63ab7f3
madhanaero <madhan@aerobyte.in>
2025-09-19 16:32:42 +0530

    updated the map

--- src/modules/contact/Map.jsx
+++ src/modules/contact/Map.jsx
@@ -1,74 +1,22 @@
-import React, { useEffect } from "react";
-import { Map, InfoWindow, GoogleApiWrapper, Marker } from "google-maps-react";
-import Geocode from "react-geocode";
-Geocode.setApiKey(import.meta.env.VITE_GOOGLE_API_KEY);
-//Geocode.setApiKey("AIzaSyAohKs5PYLzX5iweNRYOfm_ka6yCunzngo");
-Geocode.enableDebug();
+import React from "react";
 
-const MapContainer = ({
-  google,
-  locations = [{ lat: 41.998083, lng: -87.684416 }],
-}) => {
-  useEffect(() => {
-    Geocode.fromLatLng(locations[0].lat, locations[0].lng).then(
-      (response) => {
-        // console.log("fdfdfdf", response.results[0])
-        //   const address = response.results[0].formatted_address,
-        //     addressArray = response.results[0].address_components,
-        //     city = getCity(addressArray) || '',
-        //     area = getArea(address, addressArray) || '',
-        //     state = getState(addressArray) || '',
-        //     country = getCountry(addressArray) || '',
-        //     zipCode = getPostalCode(addressArray) || '',
-        //     latitude = latLng.lat,
-        //     longitude = latLng.lng;
-        //   if (!isTyped)
-        //     setAddress(`${area},${city},${state},${country}`);
-        //   return props.onPlaceSelected({
-        //     country,
-        //     state,
-        //     city,
-        //     area,
-        //     zipCode,
-        //     latitude,
-        //     longitude
-        //   });
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }, []);
+const MapContainer = () => {
   return (
-    <Map
-      google={google}
-      containerStyle={{
-        position: "static",
-        width: "100%",
-        height: "100%",
-      }}
-      center={{ lat: 41.998083, lng: -87.684416 - 0.015 }}
-      initialCenter={{ lat: 41.998083, lng: -87.684416 - 0.015 }}
-      zoom={14}
-      // disableDefaultUI={true}
-    >
-      <InfoWindow visible={true} position={{ lat: 41.998083, lng: -87.684416 }}>
-        <p>
-          {" "}
-         2144 W Devon Ave, Chicago, IL 60659, USA
-        </p>
-      </InfoWindow>
-      {locations.map((coords, index) => (
-        <Marker position={{ lat: 41.998083, lng: -87.684416 }} key={index} />
-      ))}
-    </Map>
+    <div style={{ width: "100%", height: "100%", position: "relative" }}>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2965.103210432288!2d-87.68698242487082!3d41.9980604712288!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd1c3b33e045f%3A0xe8eb7562b2d62f2d!2s2144%20W%20Devon%20Ave%20APT%202E%2C%20Chicago%2C%20IL%2060659%2C%20USA!5e0!3m2!1sen!2sin!4v1758177637046!5m2!1sen!2sin"
+        width="100%"
+        height="100%"
+        style={{ border: 0,
+           transform: "scale(110%)", 
+          transformOrigin: "top left",
+         }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
   );
 };
 
-export default GoogleApiWrapper({
-  apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
-  // apiKey.("AIzaSyCb22Xq4XjQtUAwZrOxL-hKJCWCmNwz3wc");
-
-  //Geocode.setApiKey("AIzaSyAohKs5PYLzX5iweNRYOfm_ka6yCunzngo");
-  libraries: ["geometry"],
-})(MapContainer);
+export default MapContainer;
