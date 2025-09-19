074c2e72cfd787e6d356bc52baef519f0591af0d
anwarzaki <anwarzaki856@gmail.com>
2025-09-19 17:45:14 +0530

    added more url

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173', 
+    'http://localhost:5174', 
     'http://localhost:5000',
     'http://localhost:3000',
     'http://localhost:4173',
